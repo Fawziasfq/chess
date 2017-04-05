2478da398e1e24b489733a14769e2202c1f2aaee
Fawziasfq <732598573@qq.com>
2017-04-05 15:44:26 +0800

    五子棋UI绘制

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+var chessBoard = [];
+var me = true;
+
+for (var i = 0; i < 15; i++) {
+	chessBoard[i] = [];
+	for (var j = 0; j < 15; j++) {
+		chessBoard[i][j] = 0;
+	}
+}
+
 var chess = document.getElementById('chess');
 var context = chess.getContext('2d');
 
@@ -8,9 +18,6 @@ logo.src = "img/logo1.png";
 logo.onload = function() {
 	context.drawImage(logo, 0, 0, 450, 450);
 	drawChessBoard();
-	oneStep(0, 0, true);
-	oneStep(1, 1, false);
-
 
 }
 
@@ -39,4 +46,21 @@ var oneStep = function(i, j, me) {
 
 	context.fillStyle = gradient;
 	context.fill();
+}
+
+chess.onclick = function(e) {
+	var x = e.offsetX;
+	var y = e.offsetY;
+	var i = Math.floor(x / 30);
+	var j = Math.floor(y / 30);
+	if (chessBoard[i][j] == 0) {
+		oneStep(i, j, me);
+		if (me) {
+			chessBoard[i][j] = 1;
+		} else {
+			chessBoard[i][j] = 2;
+		}
+		me = !me;
+	}
+
 }
